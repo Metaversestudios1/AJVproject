999eb898831d11f605252d7ccc2c7342b77e7827
nikita <nikita@gmail.com>
2024-10-17 17:52:59 +0530

    rank change

--- frontend/src/components/Sites/AddSite.js
+++ frontend/src/components/Sites/AddSite.js
@@ -305,7 +305,7 @@ const AddSite = () => {
                 type="text"
                 id="description"
                 className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-black block w-full p-2.5 "
-                placeholder="Enter site number"
+                placeholder="Lorem ipsum"
                 required
               />
             </div>
