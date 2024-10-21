9d2cd04f1fa7589286a2c7d77b8c03fe6fa215ae
nikita <nikita@gmail.com>
2024-10-21 17:27:17 +0530

    property edit and site add payment details done

--- frontend/src/components/Sites/AddPropertyDetails.js
+++ frontend/src/components/Sites/AddPropertyDetails.js
@@ -381,10 +381,11 @@ const AddPropertyDetails = () => {
                   name="totalValue"
                   value={data.propertyDetails.totalValue}
                   onChange={handleChange}
-                  type="text"
+                    type="number"
                   id="totalValue"
                   className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-black block w-full p-2.5 "
                   placeholder="Eg.1234.."
+                  min="1"
                   required
                 />
               </div>
@@ -393,18 +394,19 @@ const AddPropertyDetails = () => {
                   htmlFor="amountPaid"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
                 >
-                  Amount Paid
+                  First Payment
                   <span className="text-red-900 text-lg ">&#x2a;</span>
                 </label>
                 <input
                   name="amountPaid"
                   value={data.propertyDetails.amountPaid}
                   onChange={handleChange}
-                  type="text"
+                  type="number"
                   id="amountPaid"
                   className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-black block w-full p-2.5 "
                   placeholder="Eg.1200.."
                   required
+                   min="1"
                 />
               </div>
             </div>
