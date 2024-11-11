d9fcf26e9c20bcaf17235429ee36c9f6038f5796
nikita <nikita@gmail.com>
2024-11-11 15:10:21 +0530

    notification on agent/client

--- userFrontend/src/components/Sidebar.js
+++ userFrontend/src/components/Sidebar.js
@@ -214,7 +214,6 @@ const Sidebar = ({ sidebar, toggleSideBar }) => {
                   </li>
                 ))
               }
-                
             </ul>
             <li className="hs-accordion " id="users-accordion ">
                 <button
@@ -249,38 +248,7 @@ const Sidebar = ({ sidebar, toggleSideBar }) => {
                   )}
                 </button>
               </li>
-  
-              <li
-                className=" hover:scale-105 transition-transform duration-200 "
-                onClick={toggleSideBar}
-              >
-                <NavLink
-                  to="/notification"
-                  className={({ isActive }) =>
-                    isActive
-                      ? "flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-white bg-[#0472ff] rounded-lg"
-                      : "flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-white rounded-lg hover:text-black hover:bg-white"
-                  }
-                >
-                  <svg
-                    className="size-4"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  >
-                    <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
-                    <polyline points="9 22 9 12 15 12 15 22" />
-                  </svg>
-                  Notification
-                </NavLink>
-              </li>
-              
+
           </nav>
         </div>
       </div>
